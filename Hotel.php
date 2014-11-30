a38af438e1074ef1ced82f20acca31d011a676b7
nayzo <alakhefifi@gmail.com>
2014-11-30 19:54:22 +0100

    Update content

--- Hotel.php
+++ Hotel.php
@@ -2,123 +2,119 @@
 
 class Hotel
 {
-	private $x;
-	private $types = array();
-	private $result = array();
+    private $x;
+    private $types = array();
+    private $result = array();
 
-	function __construct($x, $types)
-	{
-		$this->x = $x;
-		$this->types = $types;
-		arsort($this->types);
-	}
+    function __construct($x, $types)
+    {
+        $this->x = $x;
+        $this->types = $types;
+        arsort($this->types);
+    }
 
-	function execute()
-	{
-		$buffer = $this->types;
-		while(count($buffer) > 0) {
-			$this->recherche($buffer, $this->x);
-			$this->shift($buffer);						
-		}
+    function execute()
+    {
+        $buffer = $this->types;
+        while (count($buffer) > 0) {
+            $this->recherche($buffer, $this->x);
+            $this->shift($buffer);
+        }
 
-		$this->result = array_unique($this->result);
-	}
+        $this->result = array_unique($this->result);
+    }
 
-	public function recherche($buffer, $x)
-	{
-		if($x != 0 && !empty($buffer)) {
+    public function recherche($buffer, $x)
+    {
+        if ($x != 0 && !empty($buffer)) {
 
-			$type = reset($buffer);
-			$key = key($buffer);
-			$new = $type;
+            $type = reset($buffer);
+            $key = key($buffer);
+            $new = $type;
 
-			if (count($buffer) == 1 && ($x % $type) == 0) {
-				$val = $x / $type;
-				$this->result[] = "$val $key ";
-			}
-			else {
-				while($new <= $x) {
-					$val = $new / $type;
-					$str = "$val $key ";
+            if (count($buffer) == 1 && ($x % $type) == 0) {
+                $val = $x / $type;
+                $this->result[] = "$val $key ";
+            } else {
+                while ($new <= $x) {
+                    $val = $new / $type;
+                    $str = "$val $key ";
 
-					$this->interne($buffer, $x - $new, $str);
-					$new +=$type;
-				}
-			}
-		}
+                    $this->interne($buffer, $x - $new, $str);
+                    $new += $type;
+                }
+            }
+        }
 
-	}
+    }
 
-	public function interne($buffer, $x, $str) 
-	{
-		if (count($buffer) >= 3) {
-			$this->result[] = $this->traitement($buffer, $x, $str);	
-			$this->shift($buffer);
-			$this->interne($buffer, $x, $str);			
-		}
-		else {
-			$this->result[] = $this->traitement($buffer, $x, $str);	
-		}		
-	}
+    public function interne($buffer, $x, $str)
+    {
+        if (count($buffer) >= 3) {
+            $this->result[] = $this->traitement($buffer, $x, $str);
+            $this->shift($buffer);
+            $this->interne($buffer, $x, $str);
+        } else {
+            $this->result[] = $this->traitement($buffer, $x, $str);
+        }
+    }
 
-	public function traitement($buffer, $x, $str)
-	{
-		$this->shift($buffer);
-		if($x != 0 && !empty($buffer)) {
+    public function traitement($buffer, $x, $str)
+    {
+        $this->shift($buffer);
+        if ($x != 0 && !empty($buffer)) {
 
-			$type = reset($buffer);
-			$key = key($buffer);
-			$new = $this->getNombre($x, $type);
-			$newStr = $str;
+            $type = reset($buffer);
+            $key = key($buffer);
+            $new = $this->getNombre($x, $type);
+            $newStr = $str;
 
-			if ($new == 0) {
-				return $this->traitement($buffer, $x, $str);
-			}
-			elseif (count($buffer) == 1 && ($x % $type) == 0) {
-				$val = $x / $type;
-				$str .= "$val $key ";				
-			}
-			else {
+            if ($new == 0) {
+                return $this->traitement($buffer, $x, $str);
+            } elseif (count($buffer) == 1 && ($x % $type) == 0) {
+                $val = $x / $type;
+                $str .= "$val $key ";
+            } else {
 
-					$this->traitement($buffer, $x, $str);
-					$str .= "$new $key ";
-					$autreVal = $x-($new*$type);
-					$new--;
-					while ($new > 0) {
-						
-						$chaine = $newStr . "$new $key ";
-						$var = $x-($new*$type);
-						$this->result[] = $this->traitement($buffer, $var, $chaine);
-						$new--;
-					}
+                $this->traitement($buffer, $x, $str);
+                $str .= "$new $key ";
+                $autreVal = $x - ($new * $type);
+                $new--;
+                while ($new > 0) {
 
-					return $this->traitement($buffer, $autreVal, $str);
-			}			
-		}
+                    $chaine = $newStr . "$new $key ";
+                    $var = $x - ($new * $type);
+                    $this->result[] = $this->traitement($buffer, $var, $chaine);
+                    $new--;
+                }
 
-		return $str;
-	}
+                return $this->traitement($buffer, $autreVal, $str);
+            }
+        }
 
-	public function shift(&$tab)
-	{
-		list($k) = array_keys($tab);
-		unset($tab[$k]);
-	}
+        return $str;
+    }
 
-	public function getResult()
-	{
-		return $this->result;
-	}
+    public function shift(&$tab)
+    {
+        list($k) = array_keys($tab);
+        unset($tab[$k]);
+    }
 
-	public function getNombre($x, $type)
-	{
-		if ($x >= $type) {
-			$var = $x / $type;
-			return floor($var);
-		}
+    public function getResult()
+    {
+        return $this->result;
+    }
 
-		return 0;		
-	}
+    public function getNombre($x, $type)
+    {
+        if ($x >= $type) {
+            $var = $x / $type;
+            return floor($var);
+        }
+
+        return 0;
+    }
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<script src="jquery.js"></script>
+    <script src="jquery.js"></script>
 </head>
 <body align='center'>
 
@@ -8,94 +8,94 @@
 <hr>
 
 <div>
-<h1> Systeme de repartition des personnes sur les logements d'hotel </h1>
-<br>
+    <h1> Systeme de repartition des personnes sur les logements d'hotel </h1>
+    <br>
 
-<form name="form" method="post">
-  <fieldset>
-    <legend>Formulaire:</legend>
-    <strong>Personnes:</strong>  <input type="text" size="30" name="personnes" id="personnes"><br><br>
-    <strong>Logements:</strong> <br>
+    <form name="form" method="post">
+        <fieldset>
+            <legend>Formulaire:</legend>
+            <strong>Personnes:</strong> <input type="text" size="30" name="personnes" id="personnes"><br><br>
+            <strong>Logements:</strong> <br>
 
-    <table align='center'>
-		<tr> 
-			<td> Single: <input type="checkbox" name="Single"> </td> 
-			<td> Double: <input type="checkbox" name="Double"> </td> 
-		</tr>
+            <table align='center'>
+                <tr>
+                    <td> Single: <input type="checkbox" name="Single"></td>
+                    <td> Double: <input type="checkbox" name="Double"></td>
+                </tr>
 
-		<tr> 
-			<td> Triple: <input type="checkbox" name="Triple"> </td> 
-			<td> Quadruple: <input type="checkbox" name="Quadruple"> </td> 
-		</tr>
-	</table>
+                <tr>
+                    <td> Triple: <input type="checkbox" name="Triple"></td>
+                    <td> Quadruple: <input type="checkbox" name="Quadruple"></td>
+                </tr>
+            </table>
 
-  </fieldset>
-  <br>
-  <input type="submit" value="Calculer">
-</form>
+        </fieldset>
+        <br>
+        <input type="submit" value="Calculer">
+    </form>
 
 </div>
 
 <script>
-$(document).ready(function(){
-	$('form').submit(function() {
-		var pers = $('#personnes').val();
-		if (pers=='') {
-			alert('Entrez le nombre des personnes!');
-			return false;
-		}
-		else if (!$.isNumeric(pers)) {
-			alert('Entrez un nombre de personne valide!');
-			return false;
-		}
-		else if(pers <= 0) {
-			alert('Entrez un nombre de personne superieur a zero');
-			return false;
-		}
+    $(document).ready(function () {
+        $('form').submit(function () {
+            var pers = $('#personnes').val();
+            if (pers == '') {
+                alert('Entrez le nombre des personnes!');
+                return false;
+            }
+            else if (!$.isNumeric(pers)) {
+                alert('Entrez un nombre de personne valide!');
+                return false;
+            }
+            else if (pers <= 0) {
+                alert('Entrez un nombre de personne superieur a zero');
+                return false;
+            }
 
-		var bool = false;
-		$('form input[type="checkbox"]').each(function(){
-			bool = bool || $(this).prop('checked');
-		});
+            var bool = false;
+            $('form input[type="checkbox"]').each(function () {
+                bool = bool || $(this).prop('checked');
+            });
 
-		if (!bool) {
-			alert('Selectionnez au moins un logement!');
-			return false;
-		}
+            if (!bool) {
+                alert('Selectionnez au moins un logement!');
+                return false;
+            }
 
-		return true;
+            return true;
 
-	});
-});
+        });
+    });
 
 </script>
 
-<?php 
+<?php
 require_once "Hotel.php";
 
 if (isset($_POST['personnes'])) {
-	$personnes = $_POST['personnes'];
-	$logement = array();
-	$log = '';
+    $personnes = $_POST['personnes'];
+    $logement = array();
+    $log = '';
 
-	if (isset($_POST['Single'])) {
-		$logement['Single'] = 1;
-	}
-	if (isset($_POST['Double'])) {
-		$logement['Double'] = 2;
-	}
-	if (isset($_POST['Triple'])) {
-		$logement['Triple'] = 3;
-	}
-	if (isset($_POST['Quadruple'])) {
-		$logement['Quadruple'] = 4;
-	}
+    if (isset($_POST['Single'])) {
+        $logement['Single'] = 1;
+    }
+    if (isset($_POST['Double'])) {
+        $logement['Double'] = 2;
+    }
+    if (isset($_POST['Triple'])) {
+        $logement['Triple'] = 3;
+    }
+    if (isset($_POST['Quadruple'])) {
+        $logement['Quadruple'] = 4;
+    }
 
-	foreach ($logement as $key => $value) {
-		$log .= $key . ' / ';
-	}
-	
-	echo "
+    foreach ($logement as $key => $value) {
+        $log .= $key . ' / ';
+    }
+
+    echo "
 	<table align='center'>
 	<tr> <td> <strong>Personnes:</strong> $personnes </td> </tr>
 	<tr> <td>  </td> 	</tr>
@@ -104,21 +104,21 @@ if (isset($_POST['personnes'])) {
 	</table>
 	";
 
-	echo '<span style="color:red"><h3>Resultat:</h3></span>';
+    echo '<span style="color:red"><h3>Resultat:</h3></span>';
 
 
-	$obj = new Hotel($personnes, $logement);
-	$obj->execute();
-	$resultat = $obj->getResult();
+    $obj = new Hotel($personnes, $logement);
+    $obj->execute();
+    $resultat = $obj->getResult();
 
-	$i = 1;
-	echo "<table align='center'>";
-	foreach ($resultat as $key => $value) {
-		echo "<tr> <td> <strong>Proposition $i </strong>:  $value </td> </tr>";
-		$i++;
-	}
-	echo "</table>";
-	echo "<br>";
+    $i = 1;
+    echo "<table align='center'>";
+    foreach ($resultat as $key => $value) {
+        echo "<tr> <td> <strong>Proposition $i </strong>:  $value </td> </tr>";
+        $i++;
+    }
+    echo "</table>";
+    echo "<br>";
 
 }
 ?>
